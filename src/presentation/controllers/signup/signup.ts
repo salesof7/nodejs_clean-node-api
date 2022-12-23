8b24fe993d782e34588da0a26177b81a5643b1f7
salesof7 <jpsaless2002@gmail.com>
2022-12-22 23:17:16 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -51,7 +51,8 @@ export class SignUpController implements Controller {
       });
 
       return ok(account);
-    } catch {
+    } catch (error) {
+      console.error(error);
       return serverError();
     }
   }
