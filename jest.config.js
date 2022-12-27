12d5e3438bdad5c10300c55a25c31073b4727af3
salesof7 <jpsaless2002@gmail.com>
2022-12-27 13:34:13 -0300

    chore: adjust jest config to not cover protocols

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,12 @@ module.exports = {
   // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDit>/src/main/**"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDit>/src/main/*.ts",
+    "!<rootDir>/src/**/protocols/*.ts",
+    "!<rootDir>/src/**/*-protocols.ts",
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
