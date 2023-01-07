ecb1318dde901d72e4d6227df34f783b5062902b
salesof7 <jpsaless2002@gmail.com>
2023-01-07 15:39:16 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
+  mongoUrl: process.env.MONGO_URL || "mongodb://mongo:27017/clean-node-api",
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || "Im-Batman",
 };
