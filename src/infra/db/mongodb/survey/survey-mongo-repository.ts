92d41f52ce97287ce873daacd08912a9f7c7976a
salesof7 <jpsaless2002@gmail.com>
2023-01-14 01:56:14 -0300

    feat: ensure SurveyMongoRepository add a new survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,12 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import {
+  AddSurveyModel,
+  AddSurveyRepository,
+} from "../../../../data/usecases/add-survey/db-add-survey-protocols";
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+  async add(surveyData: AddSurveyModel): Promise<void> {
+    const surveyCollection = await MongoHelper.getCollection("surveys");
+    await surveyCollection.insertOne(surveyData);
+  }
+}
