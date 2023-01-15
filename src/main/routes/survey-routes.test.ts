c833bc92cb8069aabfe64d2435cca9f9d0fecfb4
salesof7 <jpsaless2002@gmail.com>
2023-01-15 02:18:18 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
+import { sign } from "jsonwebtoken";
 import { Collection } from "mongodb";
 import request from "supertest";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
+import env from "../config/env";
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 describe("Login Routes", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as unknown as string);
@@ -16,6 +19,8 @@ describe("Login Routes", () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection("surveys");
     await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
   });
 
   describe("POST /surveys", () => {
@@ -35,6 +40,45 @@ describe("Login Routes", () => {
           ],
         })
         .expect(403);
+    });
+
+    test("should return 204 on add survey with valid accessToken", async () => {
+      const res = await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        role: "admin",
+      });
+
+      const id = res.ops[0]._id;
+      const accessToken = sign({ id }, env.jwtSecret);
+      await accountCollection.updateOne(
+        {
+          _id: id,
+        },
+        {
+          $set: {
+            accessToken,
+          },
+        }
+      );
+
+      await request(app)
+        .post("/api/surveys")
+        .set("x-access-token", accessToken)
+        .send({
+          question: "question",
+          answers: [
+            {
+              image: "http://image-name.com",
+              answer: "answer 1",
+            },
+            {
+              answer: "answer 2",
+            },
+          ],
+        })
+        .expect(204);
     });
   });
 });
