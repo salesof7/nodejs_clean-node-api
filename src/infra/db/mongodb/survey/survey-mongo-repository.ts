553b99a9d8cf937c23b3a1a7a3f43c32d6848666
salesof7 <jpsaless2002@gmail.com>
2023-01-19 01:27:42 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -3,10 +3,20 @@ import {
   AddSurveyModel,
   AddSurveyRepository,
 } from "../../../../data/usecases/add-survey/db-add-survey-protocols";
+import { LoadSurveysRepository } from "../../../../data/protocols/db/survey/load-survey-repository";
+import { SurveyModel } from "../../../../domain/models/survey";
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository
+  implements AddSurveyRepository, LoadSurveysRepository
+{
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection("surveys");
     await surveyCollection.insertOne(surveyData);
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection("surveys");
+    const surveys: SurveyModel[] = await surveyCollection.find().toArray();
+    return surveys;
   }
 }
