d6980de7c0e10411594f447c2294938e687495ed
salesof7 <jpsaless2002@gmail.com>
2023-01-30 23:14:58 -0300

    test: ensure SaveSurveyResultController returns 403 if an invalid answer is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -14,6 +14,9 @@ const makeFakeRequest = (): HttpRequest => ({
   params: {
     surveyId: "any_survey_id",
   },
+  body: {
+    answer: "any_answer",
+  },
 });
 
 const makeFakeSurvey = (): SurveyModel => ({
@@ -84,5 +87,18 @@ describe("SaveSurveyResultController", () => {
 
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test("should return 403 f an invalid answer is provided", async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle({
+      params: {
+        surveyId: "any_survey_id",
+      },
+      body: {
+        answer: "wrong_answer",
+      },
+    });
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError("answer")));
   });
 });
