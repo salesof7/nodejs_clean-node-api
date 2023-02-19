ad936e9aa54ef0113e2cac3597e1ff7583687296
salesof7 <jpsaless2002@gmail.com>
2023-02-19 12:22:05 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,7 @@
+import { loginPath } from "./paths/login-path";
+import { accountSchema } from "./schemas/account-schema";
+import { loginParamsSchema } from "./schemas/login-params-schema";
+
 export default {
   openapi: "3.0.0",
   info: {
@@ -6,4 +10,18 @@ export default {
       "API do curso do Mango para realizar enquetes entre programadores",
     version: "1.0.0",
   },
+  servers: [
+    {
+      url: "/api",
+    },
+  ],
+  tags: [
+    {
+      name: "Login",
+    },
+  ],
+  paths: {
+    "/login": loginPath,
+  },
+  schemas: { account: accountSchema, loginParams: loginParamsSchema },
 };
